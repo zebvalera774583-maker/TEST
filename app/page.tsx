4e6e7fc592044af90c9bc6fce04d2e3b3cf8cd3e
zebvalera774583-maker <zebvalera774583@gmail.com>
2025-12-28 17:39:22 +0300

    Initial commit: Next.js test project

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>ТЕСТ!!!</h1>
+
+      <button
+        style={{
+          marginTop: 20,
+          padding: "10px 20px",
+          fontSize: 16,
+        }}
+      >
+        ГРУЗИТЬ
+      </button>
+    </main>
+  );
+}
